12b6422a79b97e8b32c68a9979492819aeb3f5cd
alex2kim4chee <pecanstudio@yahoo.com>
2025-11-11 14:13:54 -0500

    modal popup form

--- web/components/ServiceForm.tsx
+++ web/components/ServiceForm.tsx
@@ -89,7 +89,7 @@ const ServiceForm: React.FC<ServiceFormProps> = ({ onSuccess }) => {
         <div className="text-center p-8 bg-green-50 border border-green-200 rounded-lg">
             <h3 className="text-2xl font-bold text-green-800">Thank You!</h3>
             <p className="mt-2 text-green-700">
-              {feedbackMessage || `Your service request has been submitted. We will contact you shortly at ${formData.phone} to confirm your appointment.`}
+              Thanks! Our dispatcher will reach out shortly to confirm.
             </p>
         </div>
     );
